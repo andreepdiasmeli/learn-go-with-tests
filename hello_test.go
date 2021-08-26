a26ab309e7b3816c1eeb94d2a0fa5acc231e0ab0
andreepdiasmeli <andre.ddias@mercadolivre.com>
2021-08-26 14:16:35 -0300

    Added default prefix to Hello method.

--- hello_test.go
+++ hello_test.go
@@ -13,6 +13,7 @@ func TestHello(t *testing.T) {
 }
  */
 
+/*
 func TestHello(t *testing.T) {
 	got := Hello("André")
 	want := "Hello, André"
@@ -20,4 +21,25 @@ func TestHello(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+ */
+
+func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("André")
+		want := "Hello, André"
+		assertCorrectMessage(t, got, want)
+		})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
