2acd5022d83dadc3b65e46b78ddf598fe598bb2f
andreepdiasmeli <andre.ddias@mercadolivre.com>
2021-08-26 14:30:32 -0300

    Added multiploe Hello languages.

--- hello_test.go
+++ hello_test.go
@@ -33,13 +33,23 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("André")
+		got := Hello("André", "")
 		want := "Hello, André"
 		assertCorrectMessage(t, got, want)
 		})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Eduardo", "Spanish")
+		want := "Hola, Eduardo"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Eduardo", "French")
+		want := "Bonjour, Eduardo"
 		assertCorrectMessage(t, got, want)
 	})
 }
